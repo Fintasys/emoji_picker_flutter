8a7ba1fe0f12b606ae1aed0ad310af6901cd711b
Stefan Humm <stef.humm@googlemail.com>
2021-03-14 20:32:55 +0900

    Improve example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emoji_picker_flutter/emoji_lists.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 
@@ -10,15 +11,10 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class CustomView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
-
 class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  bool emojiShowing = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,17 +23,75 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 200,
+        body: Column(
+          children: [
+            Expanded(child: Container()),
+            Container(
+                height: 66.0,
+                color: Colors.blue,
+                child: Row(
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            emojiShowing = !emojiShowing;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.emoji_emotions,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TextFormField(
+                            controller: _controller,
+                            style: TextStyle(
+                                fontSize: 20.0, color: Colors.black87),
+                            decoration: InputDecoration(
+                              hintText: 'Type a message',
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0,
+                                  bottom: 8.0,
+                                  top: 8.0,
+                                  right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                            )),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                          onPressed: () {
+                            // send message
+                          },
+                          icon: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          )),
+                    )
+                  ],
+                )),
+            Offstage(
+              offstage: !emojiShowing,
+              child: SizedBox(
+                height: 250,
                 child: EmojiPickerFlutter(onEmojiSelected: (category, emoji) {
-                  print(emoji);
+                  _controller.text += emoji.emoji;
+                  _controller.selection = TextSelection.fromPosition(
+                      TextPosition(offset: _controller.text.length));
                 }),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
