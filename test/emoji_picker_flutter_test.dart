8ad8e83dcbb52a312afa50c0b0ee5f8ee9b72256
stefan <Fintasys@users.noreply.github.com>
2022-01-23 14:31:18 +0900

    Add tests for skin tones

--- test/emoji_picker_flutter_test.dart
+++ test/emoji_picker_flutter_test.dart
@@ -1,7 +1,42 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:emoji_picker_flutter/src/emoji_picker_internal_utils.dart';
+import 'package:emoji_picker_flutter/src/emoji_skin_tones.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('EmojiPickerFlutter tests', (WidgetTester tester) async {
-    // Todo: Tests
+  skinToneTests();
+}
+
+void skinToneTests() {
+  final utils = EmojiPickerInternalUtils();
+  test('hasSkinTone()', () {
+    expect(utils.hasSkinTone(const Emoji('', '👍')), true);
+    expect(utils.hasSkinTone(const Emoji('', '👨‍🍳')), true);
+    expect(utils.hasSkinTone(const Emoji('', '👩‍🚀')), true);
+
+    expect(utils.hasSkinTone(const Emoji('', '🏀')), false);
+    expect(utils.hasSkinTone(const Emoji('', '😆')), false);
+    expect(utils.hasSkinTone(const Emoji('', '🧟‍♂️')), false);
+  });
+
+  test('applySkinTone()', () {
+    expect(
+      utils.applySkinTone(const Emoji('', '👍'), SkinTone.light).emoji,
+      '👍🏻',
+    );
+    expect(
+      utils.applySkinTone(const Emoji('', '🏊‍♂️'), SkinTone.mediumDark).emoji,
+      '🏊🏾‍♂️',
+    );
+    expect(
+      utils.applySkinTone(const Emoji('', '👱‍♀️'), SkinTone.dark).emoji,
+      '👱🏿‍♀️',
+    );
+  });
+
+  test('removeSkinTone()', () {
+    expect(utils.removeSkinTone(const Emoji('', '👍🏻')).emoji, '👍');
+    expect(utils.removeSkinTone(const Emoji('', '🏊🏾‍♂️')).emoji, '🏊‍♂️');
+    expect(utils.removeSkinTone(const Emoji('', '👱🏿‍♀️')).emoji, '👱‍♀️');
   });
 }
