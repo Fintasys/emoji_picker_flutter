7d6615ab20a355cc3c8abea7e93a503f673449fc
Edward DeCoste <elmdecoste@users.noreply.github.com>
2022-06-08 10:07:44 -0500

    Add the ability to put a key onto a custom widget (#77)

--- lib/src/emoji_picker_builder.dart
+++ lib/src/emoji_picker_builder.dart
@@ -6,7 +6,11 @@ import 'package:flutter/material.dart';
 /// Inhert this class to create your own EmojiPicker
 abstract class EmojiPickerBuilder extends StatefulWidget {
   /// Constructor
-  EmojiPickerBuilder(this.config, this.state);
+  EmojiPickerBuilder(
+    this.config,
+    this.state, {
+    Key? key,
+  }) : super(key: key);
 
   /// Config for customizations
   final Config config;
