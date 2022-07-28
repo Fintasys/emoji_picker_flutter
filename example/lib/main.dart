2d8d2990593879582db685eac10833d2a6105381
Yendo <it@yendoplan.com>
2022-07-28 21:04:14 +0200

    moving advanced integration example into a separate file, keeping the default example simple

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,67 +14,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _editKey = GlobalKey();
-  final _focusNode = FocusNode();
-  final TextEditingController _controller =
-      EmojiTextEditingController(emojiStyle: GoogleFonts.notoEmoji());
+  final TextEditingController _controller = TextEditingController();
   bool emojiShowing = false;
-
-  void _updateTextEditingValue(TextEditingValue value) {
-    (_editKey.currentState as TextSelectionGestureDetectorBuilderDelegate)
-        .editableTextKey
-        .currentState
-        ?.userUpdateTextEditingValue(value, SelectionChangedCause.keyboard);
-  }
-
-  /// This demostrates advanced handling of the seleted emoji.
-  /// Updating TextEditingValue this way ensures that the underlying
-  /// EditableText will scroll to display caret position if necessary.
-  /// Simply updating controller text and selection properties does not achieve
-  /// that.
-  /// One of the limitations of this approach is that it cannot be used with
-  /// [TextFormField] widgets since they don't provide a way to reach their
-  /// internal TextField widget state.
-  ///
-  /// You can always fall back to basic integration by just setting
-  /// [textEditingController] parameter in the [EmojiPicker] constructor.
-  _onEmojiSelected(Emoji emoji) {
-    print('_onEmojiSelected: ${emoji.emoji}');
-
-    if (_controller.selection.base.offset < 0) {
-      _updateTextEditingValue(TextEditingValue(
-        text: _controller.text + emoji.emoji,
-      ));
-      return;
-    }
-
-    final selection = _controller.selection;
-    final newText = _controller.text
-        .replaceRange(selection.start, selection.end, emoji.emoji);
-    final emojiLength = emoji.emoji.length;
-    _updateTextEditingValue(TextEditingValue(
-        text: newText,
-        selection: selection.copyWith(
-          baseOffset: selection.start + emojiLength,
-          extentOffset: selection.start + emojiLength,
-        )));
-  }
-
-  _onBackspacePressed() {
-    print('_onBackspacePressed');
-    if (_controller.selection.base.offset < 0) {
-      return;
-    }
-
-    final selection = _controller.value.selection;
-    final text = _controller.value.text;
-    final newTextBeforeCursor =
-        selection.textBefore(text).characters.skipLast(1).toString();
-    _updateTextEditingValue(TextEditingValue(
-        text: newTextBeforeCursor + selection.textAfter(text),
-        selection: TextSelection.fromPosition(
-            TextPosition(offset: newTextBeforeCursor.length))));
-  }
 
   @override
   void dispose() {
@@ -94,20 +34,6 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             const Spacer(),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                      style:
-                          const TextStyle(color: Colors.black, fontSize: 18.0),
-                      children: EmojiPickerUtils().setEmojiTextStyle(
-                          '⌨ This text demonstrates how you can include '
-                          'custom-font-based emojis 😁 '
-                          'in your static texts 🎉👏',
-                          emojiStyle: GoogleFonts.notoEmoji(
-                              color: Colors.blueAccent)))),
-            ),
             Container(
                 height: 66.0,
                 color: Colors.blue,
@@ -119,18 +45,6 @@ class _MyAppState extends State<MyApp> {
                         onPressed: () {
                           setState(() {
                             emojiShowing = !emojiShowing;
-                            if (emojiShowing) {
-                              // If TextField remains focused, the keyboard
-                              // will pop up on every emoji insert done with
-                              // EditableTextState manipulation.
-
-                              // In a production app you might want to detect
-                              // keyboard opens and hide emoji picker
-                              // for more consistent experience.
-                              _focusNode.unfocus();
-                            } else {
-                              _focusNode.requestFocus();
-                            }
                           });
                         },
                         icon: const Icon(
@@ -143,9 +57,7 @@ class _MyAppState extends State<MyApp> {
                       child: Padding(
                         padding: const EdgeInsets.symmetric(vertical: 8.0),
                         child: TextField(
-                            key: _editKey,
                             controller: _controller,
-                            focusNode: _focusNode,
                             style: const TextStyle(
                                 fontSize: 20.0, color: Colors.black87),
                             decoration: InputDecoration(
@@ -181,10 +93,7 @@ class _MyAppState extends State<MyApp> {
               child: SizedBox(
                   height: 250,
                   child: EmojiPicker(
-                    onEmojiSelected: (Category category, Emoji emoji) {
-                      _onEmojiSelected(emoji);
-                    },
-                    onBackspacePressed: _onBackspacePressed,
+                    textEditingController: _controller,
                     config: Config(
                       columns: 7,
                       // Issue: https://github.com/flutter/flutter/issues/28894
@@ -212,18 +121,7 @@ class _MyAppState extends State<MyApp> {
                       tabIndicatorAnimDuration: kTabScrollDuration,
                       categoryIcons: const CategoryIcons(),
                       buttonMode: ButtonMode.MATERIAL,
-                      checkPlatformCompatibility: false,
-                      emojiTextStyle:
-                          GoogleFonts.notoEmoji(color: Colors.black),
-                      // or TextStyle(fontFamily: 'NotoColorEmoji',
-                      //              color: Colors.blueAccent)
-
-                      /// example: using only first 10 emojis
-                      /// from each category:
-                      // emojiSet: defaultEmojiSet
-                      //     .map((e) =>
-                      //         e.copyWith(emoji: e.emoji.take(10).toList()))
-                      //     .toList()
+                      checkPlatformCompatibility: true,
                     ),
                   )),
             ),
