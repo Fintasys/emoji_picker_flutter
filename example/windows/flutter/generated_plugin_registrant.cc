820526fb3cca848fd4b6e6a5735c4158cc6d8336
Stefan Humm <Fintasys@users.noreply.github.com>
2022-09-29 08:42:59 -0700

    Add windows to example

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <emoji_picker_flutter/emoji_picker_flutter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  EmojiPickerFlutterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("EmojiPickerFlutterPlugin"));
+}
