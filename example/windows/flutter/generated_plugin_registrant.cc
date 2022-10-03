2c68231091f8fce6306213980e1c81e5fd77ad3a
Stefan Humm <Fintasys@users.noreply.github.com>
2022-10-03 09:15:22 -0700

    Add missing files

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <emoji_picker_flutter/emoji_picker_flutter_plugin.h>
+#include <emoji_picker_flutter/emoji_picker_flutter_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  EmojiPickerFlutterPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("EmojiPickerFlutterPlugin"));
+  EmojiPickerFlutterPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("EmojiPickerFlutterPluginCApi"));
 }
---
+++ lib/emoji_picker_flutter_method_channel.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'emoji_picker_flutter_platform_interface.dart';
+
+/// An implementation of [EmojiPickerFlutterPlatform] that uses method channels.
+class MethodChannelEmojiPickerFlutter extends EmojiPickerFlutterPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('emoji_picker_flutter');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+}
