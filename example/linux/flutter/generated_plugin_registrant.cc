edbf46f3f9310a5888f8b1c75aa677bd28b28262
Stefan Humm <Fintasys@users.noreply.github.com>
2022-10-17 14:06:40 +0900

    Add Linux Support  (#110)

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,15 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <emoji_picker_flutter/emoji_picker_flutter_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) emoji_picker_flutter_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "EmojiPickerFlutterPlugin");
+  emoji_picker_flutter_plugin_register_with_registrar(emoji_picker_flutter_registrar);
+}
