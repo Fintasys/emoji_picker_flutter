47f161550e0202229f4eb22ec1adc3e8863ee513
Stefan Humm <Fintasys@users.noreply.github.com>
2023-05-31 22:31:37 +0900

    Change ReadMe and Version

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -121,7 +121,7 @@ class _MyAppState extends State<MyApp> {
                       skinToneDialogBgColor: Colors.white,
                       skinToneIndicatorColor: Colors.grey,
                       enableSkinTones: true,
-                      recentTabBehavior: RecentTabBehavior.POPULAR,
+                      recentTabBehavior: RecentTabBehavior.RECENT,
                       recentsLimit: 28,
                       replaceEmojiOnLimitExceed: false,
                       noRecents: const Text(
