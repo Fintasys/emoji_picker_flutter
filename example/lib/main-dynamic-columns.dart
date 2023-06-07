3618170094f2f30cf8d00146185c7196799ba3e7
Tim Maffett <timmaffett@gmail.com>
2023-06-07 07:48:01 -0700

    Add example app make number of emoji columns dynamic based on screen width (#134)

---
+++ example/lib/main-dynamic-columns.dart
@@ -0,0 +1,143 @@
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/// Example for EmojiPickerFlutter
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  bool emojiShowing = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final emojiPadding =
+        9 * 2; // 9 pixels on both left and right sides of each emoji.
+    final emojiSize = 32 *
+        (foundation.defaultTargetPlatform ==
+                TargetPlatform
+                    .iOS // Issue: https://github.com/flutter/flutter/issues/28894
+            ? 1.30
+            : 1.0);
+    final numEmojiColumns =
+        (screenSize.width / (emojiSize + emojiPadding)).floor();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Emoji Picker Example App'),
+        ),
+        body: Column(
+          children: [
+            const Spacer(),
+            Container(
+                height: 66.0,
+                color: Colors.blue,
+                child: Row(
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            emojiShowing = !emojiShowing;
+                          });
+                        },
+                        icon: const Icon(
+                          Icons.emoji_emotions,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TextField(
+                            controller: _controller,
+                            style: const TextStyle(
+                                fontSize: 20.0, color: Colors.black87),
+                            decoration: InputDecoration(
+                              hintText: 'Type a message',
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0,
+                                  bottom: 8.0,
+                                  top: 8.0,
+                                  right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                            )),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                          onPressed: () {
+                            // send message
+                          },
+                          icon: const Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          )),
+                    )
+                  ],
+                )),
+            Offstage(
+              offstage: !emojiShowing,
+              child: SizedBox(
+                  height: 250,
+                  child: EmojiPicker(
+                    textEditingController: _controller,
+                    config: Config(
+                      columns: numEmojiColumns,
+                      emojiSizeMax: emojiSize,
+                      verticalSpacing: 0,
+                      horizontalSpacing: 0,
+                      gridPadding: EdgeInsets.zero,
+                      initCategory: Category.RECENT,
+                      bgColor: const Color(0xFFF2F2F2),
+                      indicatorColor: Colors.blue,
+                      iconColor: Colors.grey,
+                      iconColorSelected: Colors.blue,
+                      backspaceColor: Colors.blue,
+                      skinToneDialogBgColor: Colors.white,
+                      skinToneIndicatorColor: Colors.grey,
+                      enableSkinTones: true,
+                      showRecentsTab: true,
+                      recentsLimit: 28,
+                      replaceEmojiOnLimitExceed: false,
+                      noRecents: const Text(
+                        'No Recents',
+                        style: TextStyle(fontSize: 20, color: Colors.black26),
+                        textAlign: TextAlign.center,
+                      ),
+                      loadingIndicator: const SizedBox.shrink(),
+                      tabIndicatorAnimDuration: kTabScrollDuration,
+                      categoryIcons: const CategoryIcons(),
+                      buttonMode: ButtonMode.MATERIAL,
+                      checkPlatformCompatibility: true,
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
