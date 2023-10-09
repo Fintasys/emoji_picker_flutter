a0f67fed297e02c43d16b22b44db691c53c52138
Stefan Humm <Fintasys@users.noreply.github.com>
2023-10-09 14:49:05 +0900

    update iOS project

--- ios/Classes/EmojiPickerFlutterPlugin.swift
+++ ios/Classes/EmojiPickerFlutterPlugin.swift
@@ -1,14 +1,19 @@
 import Flutter
 import UIKit
 
-public class SwiftEmojiPickerFlutterPlugin: NSObject, FlutterPlugin {
+public class EmojiPickerFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "emoji_picker_flutter", binaryMessenger: registrar.messenger())
-    let instance = SwiftEmojiPickerFlutterPlugin()
+    let instance = EmojiPickerFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "getPlatformVersion":
+      result("iOS " + UIDevice.current.systemVersion)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 }
