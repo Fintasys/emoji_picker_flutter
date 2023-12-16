99b92dd388d57eb9ce8d233cf010d65a66695b0b
vedasjad <96325125+vedasjad@users.noreply.github.com>
2023-12-16 13:06:41 +0530

    Delete word by word after 3 seconds have passed since pressed

--- lib/src/default_emoji_picker_view.dart
+++ lib/src/default_emoji_picker_view.dart
@@ -211,15 +211,48 @@ class _DefaultEmojiPickerViewState extends State<DefaultEmojiPickerView>
     closeSkinToneOverlay();
   }
 
+  /// Start the callback for long-pressing the backspace button.
   void _startOnBackspacePressedCallback() {
-    const callbackInterval = Duration(milliseconds: 75);
+    // Initial callback interval for short presses
+    var callbackInterval = const Duration(milliseconds: 75);
+    var millisecondsSincePressed = 0;
+
+    // Callback function executed on each timer tick
+    void _callback(Timer timer) {
+      // Accumulate elapsed time since the last tick
+      millisecondsSincePressed += callbackInterval.inMilliseconds;
+
+      // If the long-press duration exceeds 3 seconds
+      if (millisecondsSincePressed > 3000 &&
+          callbackInterval == const Duration(milliseconds: 75)) {
+        // Switch to a longer callback interval for word-by-word deletion
+        callbackInterval = const Duration(milliseconds: 300);
+
+        // Cancel the existing timer and start a new one with the updated interval
+        _onBackspacePressedCallbackTimer?.cancel();
+        _onBackspacePressedCallbackTimer =
+            Timer.periodic(callbackInterval, _callback);
+
+        // Reset the elapsed time for the new interval
+        millisecondsSincePressed = 0;
+      }
+
+      // Trigger the appropriate callback based on the interval
+      if (callbackInterval == const Duration(milliseconds: 75)) {
+        widget.state.onBackspacePressed!(); // Short-press callback
+      } else {
+        widget.state.onBackspaceLongPressed(); // Long-press callback
+      }
+    }
+
+    // Start the initial timer with the short-press interval
     _onBackspacePressedCallbackTimer =
-        Timer.periodic(callbackInterval, (timer) {
-      widget.state.onBackspacePressed!();
-    });
+        Timer.periodic(callbackInterval, _callback);
   }
 
+  /// Stop the callback for long-pressing the backspace button.
   void _stopOnBackspacePressedCallback() {
+    // Cancel the active timer
     _onBackspacePressedCallbackTimer?.cancel();
   }
 }
