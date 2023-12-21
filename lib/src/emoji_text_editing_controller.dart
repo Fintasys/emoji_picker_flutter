45d229b1c7d683615a11e487e2fb0c8b5f3a58e1
Stefan Humm <Fintasys@users.noreply.github.com>
2023-12-21 23:38:49 +0100

    Fix issue with skin colors and controller

--- lib/src/emoji_text_editing_controller.dart
+++ lib/src/emoji_text_editing_controller.dart
@@ -1,6 +1,9 @@
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/widgets.dart';
 
+/// Default delimiter for regex
+const delimiter = '|';
+
 /// Text editing controller that produces text spans on the fly for setting
 /// a particular style to emoji characters. Offloads the main magic to
 /// [EmojiPickerUtils.setEmojiTextStyle] method.
@@ -8,11 +11,25 @@ class EmojiTextEditingController extends TextEditingController {
   /// Constructor, requres emojiStyle, since otherwise this class has no effect
   EmojiTextEditingController({String? text, required this.emojiStyle})
       : super(text: text) {
-    var emojis = '';
+    // Combine all emojis as regex string
+    final utils = EmojiPickerUtils();
+    final regExBuffer = StringBuffer();
     for (final list in defaultEmojiSet) {
-      emojis += list.emoji.map((e) => e.emoji).join('|');
+      for (final emoji in list.emoji) {
+        // Put emoji inclusive skin tone before actualy emoji
+        // to avoid color and emoji being seperated
+        if (emoji.hasSkinTone) {
+          for (final skinTone in SkinTone.values) {
+            regExBuffer.write(
+              '${utils.applySkinTone(emoji, skinTone).emoji}$delimiter',
+            );
+          }
+        }
+        regExBuffer.write('${emoji.emoji}$delimiter');
+      }
     }
-    _regex = RegExp(emojis);
+    final regExString = regExBuffer.toString();
+    _regex = RegExp(regExString.substring(0, regExString.length - 1));
   }
 
   /// The style used for the emoji characters
