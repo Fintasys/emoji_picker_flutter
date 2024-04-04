eea4f1add89cfc8a9d702068d1819d8d268588ba
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-04-04 13:37:13 +0100

    Switched from `dart.html` to `package:web`
    Allows WASM builds

--- lib/emoji_picker_flutter_web.dart
+++ lib/emoji_picker_flutter_web.dart
@@ -2,9 +2,8 @@
 // of your plugin as a separate package, instead of inlining it in the same
 // package as the core of your plugin.
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
-
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:web/web.dart' as html show window;
 
 import 'emoji_picker_platform_interface.dart';
 
